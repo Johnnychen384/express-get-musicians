cdbcd25df38b3941388e19749a42d96092f6ee4b
Johnny Chen <Johnnychen384@gmail.com>
2023-03-08 13:04:21 -0500

    added routes for delete/create/update

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
-
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //TODO
 app.get("/musicians", async (req, res) => {
@@ -15,6 +17,49 @@ app.get("/musicians/:id", async (req, res) => {
     const target = await Musician.findByPk(req.params.id)
     res.json(target)
 })
+
+app.post("/musicians", async (req, res) => {
+
+    try{
+        const formData = await Musician.create(req.body)
+        res.status(200).json(formData)
+    } catch(error) {
+        console.error(error)
+        res.status(404).send("Could not create new instance.")
+    }
+    
+})
+
+
+app.put("/musicians/:id", async (req, res) => {
+    try{
+        const target = await Musician.findByPk(req.params.id)
+        const {name, instrument} = req.body
+        await target.update({name: name, instrument: instrument})
+        res.status(200).json(target)
+
+    } catch(error) {
+        console.error(error)
+        res.status(404).send("Could not create new instance.")
+    }
+})
+
+
+app.delete("/musicians/:id", async (req, res) => {
+    try{
+
+        const target = await Musician.findByPk(req.params.id)
+        console.log("Found target: ", target)
+        await target.destroy()
+        res.status(200).send("Successfully deleted.")
+        
+
+    } catch(error) {
+        console.log(error)
+        res.status(404).send("Could not create new instance.")
+    }
+})
+
 
 
 app.listen(port, () => {
